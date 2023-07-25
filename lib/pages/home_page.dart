d1cb1794d8ce8f35c4a5c781bec0950747f8dec7
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2023-07-25 11:02:44 +0530

    Cart badge add and Update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_application_1/models/cart.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/pages/routes.dart';
 import 'dart:convert';
 import 'package:velocity_x/velocity_x.dart';
 
+import '../core/store.dart';
 import '../widgets/home_widgets/catalog_header.dart';
 import '../widgets/home_widgets/catalog_list.dart';
 
@@ -39,12 +41,22 @@ class _HomePageState extends State<HomePage> {
 //j_d_a
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
         backgroundColor: Colors.blue.shade100,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-          backgroundColor: Colors.black,
-          child: const Icon(CupertinoIcons.cart),
+        floatingActionButton: VxBuilder(
+          mutations: const {AddMutation, RemoveMutation},
+          builder: (context, _, __) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: Colors.black,
+            child: const Icon(CupertinoIcons.cart),
+          ).badge(
+            color: Vx.red500,
+            size: 20,
+            count: _cart.items.length,
+            textStyle: const TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold),
+          ),
         ),
         body: SafeArea(
           child: Container(
